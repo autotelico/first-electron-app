1bfe773afc9126af99b50c1f269c775fbc88ad6f
Henrique Magalhães <henriqueheron1@gmail.com>
2025-08-15 19:13:11 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# First Electron App
+
+This app:
+- Runs React on a localhost port
+- Runs Electron, displaying the content being sent to that port
+
+Still learning how to write this in Prod but I believe I'll have to read up on [electron-builder](https://www.electron.build/index.html)
---
+++ electron.js
@@ -0,0 +1,38 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  const startURL = 
+  'http://localhost:5173' 
+  // `file://${path.join(__dirname, '../first-electron/index.html')}`;
+
+  console.log('the url is ', startURL)
+
+  mainWindow.loadURL(startURL);
+
+  mainWindow.on('closed', () => (mainWindow = null));
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
